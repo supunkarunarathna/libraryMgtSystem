4bea1aaef984d719dc767a2f3b8a3fe523620722
supunkarunarathna <supunkarunarathna92@gmail.com>
2024-12-22 00:36:30 +0800

    commit property files

---
+++ src/test/java/com/octcode/library/libraryMgtSystem/LibraryMgtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octcode.library.libraryMgtSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMgtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
